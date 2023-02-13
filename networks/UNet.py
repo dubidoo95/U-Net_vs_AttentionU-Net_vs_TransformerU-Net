14b1dab094de1bcd98a131f42d3e243fdb912051
dubidoo95 <dubidoo95@gmail.com>
2023-02-13 11:47:35 +0900

    U-Net code

--- networks/UNet.py
+++ networks/UNet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Conv_block(nn.Module):
     """
     Create conv block with convoluion layer, batchnormalization and relu function to use in U-Net network.
@@ -23,7 +24,7 @@ class Conv_block(nn.Module):
 
 class UNet(nn.Module):
     """
-    In encoding, network makes 5 features(enc1, enc2, enc3, enc4, bottom).
+    In downsampling, network makes 5 features(enc1, enc2, enc3, enc4, bottom).
     Bottom is merged with encs(enc4, enc3, enc2, enc1 in order) and enters decoding process.
     It is restored to the image size with positional features through a series of upconv layers.
     """
@@ -55,22 +56,18 @@ class UNet(nn.Module):
         self.dec1 = nn.Sequential(
                         Conv_block(in_channels=1024, out_channels=512),
                         Conv_block(in_channels=512, out_channels=256))
-
         self.upconv2 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=2, stride=2)
         self.dec2 = nn.Sequential(
                         Conv_block(in_channels=512, out_channels=256),
                         Conv_block(in_channels=256, out_channels=128))
-
         self.upconv3 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=2, stride=2)
         self.dec3 = nn.Sequential(
                         Conv_block(in_channels=256, out_channels=128),
                         Conv_block(in_channels=128, out_channels=64))
-
         self.upconv4 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2)
         self.dec4 = nn.Sequential(
                         Conv_block(in_channels=128, out_channels=64),
                         Conv_block(in_channels=64, out_channels=64))
-        
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0) # (B, C, H, W) -> (B, C, H/2, W/2). Contracting image size to extract features.
         self.fc = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=1, stride=1, padding=0, bias=True)
 
@@ -107,5 +104,4 @@ class UNet(nn.Module):
         out = self.dec4(out) # (B, 64, H, W)
         out = self.fc(out) # (B, 1, H, W)
         out = torch.sigmoid(out)
-        
         return out
